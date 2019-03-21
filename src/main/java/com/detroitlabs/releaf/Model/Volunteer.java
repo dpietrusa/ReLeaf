acd1e0f05f9653cef70a641ca9f7ce04f0de09bc
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-03-21 16:33:57 -0400

    Create Volunteer class, volunteer repository, and volunteer controller.

---
+++ src/main/java/com/detroitlabs/releaf/Model/Volunteer.java
@@ -0,0 +1,76 @@
+package com.detroitlabs.releaf.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Volunteer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String name;
+    private String email;
+    private String phone;
+    private String personalDescription;
+    private String dateAvailable;
+    private String volunteerOffering;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPersonalDescription() {
+        return personalDescription;
+    }
+
+    public void setPersonalDescription(String personalDescription) {
+        this.personalDescription = personalDescription;
+    }
+
+    public String getDateAvailable() {
+        return dateAvailable;
+    }
+
+    public void setDateAvailable(String dateAvailable) {
+        this.dateAvailable = dateAvailable;
+    }
+
+    public String getVolunteerOffering() {
+        return volunteerOffering;
+    }
+
+    public void setVolunteerOffering(String volunteerOffering) {
+        this.volunteerOffering = volunteerOffering;
+    }
+}
