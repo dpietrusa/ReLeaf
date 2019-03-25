de7394f5a74df69f9c3976ea2a920d4ff5313a2f
Mikayla Clemons <mikaylaclemons@Mikaylas-MacBook-Pro.local>
2019-03-25 13:45:51 -0400

    Create HomePageDetails class for the details needed for the index html template.

--- src/main/java/com/detroitlabs/releaf/Controller/DisasterController.java
+++ src/main/java/com/detroitlabs/releaf/Controller/DisasterController.java
@@ -2,6 +2,7 @@ package com.detroitlabs.releaf.Controller;
 
 import com.detroitlabs.releaf.Model.AllDisastersWrapper;
 import com.detroitlabs.releaf.Model.DisasterData;
+import com.detroitlabs.releaf.Model.HomePageDetails;
 import com.detroitlabs.releaf.Service.ReleafWebService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -22,24 +23,12 @@ public class DisasterController {
 
 
     @RequestMapping("/")
+    @ResponseBody
     public String displayDisasters(ModelMap modelMap) {
-        AllDisastersWrapper allDisastersWrapper = releafWebService.fetchDisasterData();
-        List<DisasterData> disasterData = allDisastersWrapper.getDisasterDataList();
-////   return allDisastersDetailWrapper.getDisasterDataList().get(0).getFields().getDescription();
-//        allDisastersWrapper.getDisasterDataList().get(0).getId();
-        modelMap.put("disasterData", disasterData);
-
-        String id = null;
-        for(DisasterData data : disasterData){
-          id = data.getId();
-        }
-
-
-        AllDisastersWrapper allDisastersDetailWrapper = releafWebService.fetchDisasterDetailData(id);
-        List<DisasterData> disasterDetails = allDisastersDetailWrapper.getDisasterDataList();
-        modelMap.put("disasterDetails", disasterDetails);
-
-        return "index";
+        HomePageDetails homePage = releafWebService.fetchDisasterData();
+        List<DisasterData> disasterData = homePage.getAllDisastersWrapper().getDisasterDataList();
+        return disasterData.get(0).getFields().getName();
+//        return "index";
 
     }
 
--- src/main/java/com/detroitlabs/releaf/Service/ReleafWebService.java
+++ src/main/java/com/detroitlabs/releaf/Service/ReleafWebService.java
@@ -1,14 +1,15 @@
 package com.detroitlabs.releaf.Service;
 
 import com.detroitlabs.releaf.Model.AllDisastersWrapper;
+import com.detroitlabs.releaf.Model.HomePageDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Component
 public class ReleafWebService {
-    public AllDisastersWrapper fetchDisasterData(){
+    public HomePageDetails fetchDisasterData(){
         RestTemplate restTemplate = new RestTemplate();
-        return restTemplate.getForObject("https://api.reliefweb.int/v1/disasters?appname=releaf&sort=date:desc", AllDisastersWrapper.class);
+        return restTemplate.getForObject("https://api.reliefweb.int/v1/disasters?appname=releaf&sort=date:desc", HomePageDetails.class);
     }
 
     public AllDisastersWrapper fetchDisasterDetailData(String id){
